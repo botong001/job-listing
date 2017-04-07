247c7f092f65dc741069eda975ac474b4f669871
wuxiao <wudawuxiao1375@163.com>
2017-04-07 11:46:40 +0800

    add_column

--- db/migrate/20170407020821_add_more_detail_to_job.rb
+++ db/migrate/20170407020821_add_more_detail_to_job.rb
@@ -1,7 +1,7 @@
 class AddMoreDetailToJob < ActiveRecord::Migration[5.0]
-  def change
-    add_columu :jobs, :wage_upper_bound, :integer
-    add_columu :jobs, :wage_lower_bound, :integer
-    add_columu :jobs, :contact_email, :string
+  def chang
+    add_column :jobs, :wage_upper_bound, :integer
+    add_column :jobs, :wage_lower_bound, :integer
+    add_column :jobs, :contact_email, :string
   end
 end
